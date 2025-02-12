c8d699d0d840d32040da6bcfb02bdb35b8de966d
PRAVEEN <praveen270304@gmail.com>
2025-02-12 14:05:48 +0530

    Add files via upload

---
+++ Calcutator final/main.js
@@ -0,0 +1,37 @@
+
+        let expression = '';
+        let input =   document.getElementById('display')
+
+        function addVal(value) {
+            expression += value;
+            input.value = expression;
+        }
+
+        function clearVal() {
+            expression = '';
+           input.value = '';
+        }
+
+        function calculateVal() {
+            try {
+                let result = new Function('return ' + expression)();
+                input.value = result;
+                expression = result.toString();
+            } catch (error) {
+                input.value = 'Error';
+            }
+        }
+   
+        function percent(){
+            input.value /= 100
+            expression = input.value
+       } 
+
+
+       function delVal(){
+    
+        input.value =  input.value.slice(0, length -1)
+        expression = input.value
+   }
+
+   
